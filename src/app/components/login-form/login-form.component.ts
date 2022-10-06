1846d47e54f536cdc6a9cc0a1919042d9a267a21
Susana Esparza <susana.esparza1213@gmail.com>
2022-10-06 11:12:18 -0500

    changes

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,17 +1,17 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent implements OnChanges{
+export class LoginFormComponent implements OnChanges {
 
   @Input() isSignUp!: boolean;
 
   formUser!: FormGroup;
-  
+
   defaultFields = {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
@@ -24,18 +24,18 @@ export class LoginFormComponent implements OnChanges{
 
   constructor(
     private fb: FormBuilder
-  ) { 
-    
+  ) {
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.initForm();
     console.log(changes);
+    this.initForm();
   }
 
-  initForm(){
+  initForm() {
     let userFields = { ...this.defaultFields };
-    if(this.isSignUp){
+    if (this.isSignUp) {
       userFields = { ...this.defaultFields, ...this.extraFields };
     }
     this.formUser = this.fb.group({
@@ -43,8 +43,8 @@ export class LoginFormComponent implements OnChanges{
     })
   }
 
-  onSubmit(){
-    if(this.formUser.invalid){
+  onSubmitForm() {
+    if (this.formUser.invalid) {
       alert("debe ingresar todos los inputs");
       return;
     }
